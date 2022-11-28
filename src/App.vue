5fb9a3b1e0b71fde628d820e9d880a251a9b7293
Huyhoang <hoangb1913024@student.ctu.edu.vn>
2022-11-29 03:05:06 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!-- <script>
+export default {
+}
+</script>
+<template>
+  <h1>Hello, Vue.js!</h1>
+</template>
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style> -->
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+<template>
+  <AppHeader />
+  <div class="container mt-3 flex-column">
+    <router-view />
+  </div>
+</template>
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
